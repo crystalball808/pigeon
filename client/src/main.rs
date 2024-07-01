3866b43d2ddd9f17019a9eb7587a8855e227e3d2
Roman <romagritcaenko@gmail.com>
2024-07-01 21:38:42 +0300

    remove comments

--- client/src/main.rs
+++ client/src/main.rs
@@ -121,36 +121,3 @@ async fn run_app<B: Backend>(
         }
     }
 }
-
-// let mut stream = TcpStream::connect("localhost:8080").await?;
-//
-// let (reader, mut writer) = stream.split();
-//
-// let mut buf_reader = BufReader::new(reader);
-// let mut buffer = String::new();
-//
-// let mut input_reader = BufReader::new(io::stdin());
-// let mut input_buffer = String::new();
-//
-// loop {
-//     tokio::select! {
-//         // send message
-//         result = input_reader.read_line(&mut input_buffer) => {
-//             let bytes_read = result?;
-//             if bytes_read == 0 {
-//                 continue
-//             }
-//             writer.write_all(input_buffer.as_bytes()).await?;
-//             input_buffer.clear();
-//         }
-//         // print received message
-//         result = buf_reader.read_line(&mut buffer) => {
-//             let bytes_read = result?;
-//             if bytes_read == 0 {
-//                 continue
-//             }
-//             println!("message - {buffer}");
-//             buffer.clear();
-//         }
-//     }
-// }
